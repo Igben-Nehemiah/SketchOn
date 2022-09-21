616e83698c86b15de6c2be15bc45d99c49649977
nehemiah <igbennehemiah@gmail.com>
2022-09-21 23:39:52 +0100

    Modifed imports

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,30 @@
 import { createLeftPane } from "./layouts/leftpane";
-import { createCanvas } from "./layouts/canvas";
+import {
+    createCanvas,
+    resizeCanvas,
+    setDrawingProperties,
+} from "./layouts/canvas";
+import {
+    createRightPane,
+    rightPaneExpanded,
+    RightPaneProperties,
+} from "./layouts/right-pane";
 
-createLeftPane();
+const main = document.querySelector("main")!;
 createCanvas();
+createLeftPane();
+createRightPane();
+
+main.addEventListener("collapse", (e: Event) => {
+    // main.classList.toggle("full-canvas");
+    // console.log(e.details);
+    var eee = e as CustomEvent<{ collapsed: boolean }>;
+    console.log(eee.detail.collapsed);
+    resizeCanvas();
+});
+
+main.addEventListener("propertiesChange", (e: Event) => {
+    console.log("In the main");
+    var eee = e as CustomEvent<{ rightPaneProperties: RightPaneProperties }>;
+    setDrawingProperties(eee.detail.rightPaneProperties);
+});
