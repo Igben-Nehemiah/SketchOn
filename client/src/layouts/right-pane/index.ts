1044de3ce3e81c1977d234ed861e3d0881240a20
nehemiah <igbennehemiah@gmail.com>
2022-09-21 23:36:36 +0100

    Added custom right pane event

--- client/src/layouts/right-pane/index.ts
+++ client/src/layouts/right-pane/index.ts
@@ -1,49 +1,116 @@
-const Collapse = new CustomEvent("collapse", {
-    detail: {},
+type RightPaneProperties = {
+    strokeStyle: string;
+    fillStyle: string;
+    lineWidth: number;
+};
+
+//
+const rightPaneProperties: RightPaneProperties = {
+    fillStyle: "#000000",
+    strokeStyle: "#000000",
+    lineWidth: 1.0,
+};
+
+// Events
+let rightPaneExpanded: boolean = true;
+const collapse = new CustomEvent<{ collapsed: boolean }>("collapse", {
+    detail: {
+        collapsed: rightPaneExpanded,
+    },
     bubbles: true,
     cancelable: true,
     composed: false,
 });
 
-let rightPaneExpanded: boolean;
+const propertiesChange = new CustomEvent<{
+    rightPaneProperties: RightPaneProperties;
+}>("propertiesChange", {
+    detail: {
+        rightPaneProperties: rightPaneProperties,
+    },
+    bubbles: true,
+    cancelable: true,
+    composed: false,
+});
 
 function createRightPane() {
-    const chat = document.querySelector(".chat")!;
-    const properties = document.querySelector(".properties")!;
     const rightPane = document.querySelector(".right-pane")!;
-    const canvas = (document.querySelector("#canvas") as HTMLCanvasElement)!;
+    const chatTab = document.querySelector(".chat-tab")!;
+    const propertiesTab = document.querySelector(".properties-tab")!;
+
+    // Buttons
     const propertiesBtn = (document.getElementById(
         "properties"
     ) as HTMLButtonElement)!;
-
-    const expandBtn = (document.getElementById("expand") as HTMLButtonElement)!;
-
     const chatBtn = (document.getElementById("chat") as HTMLButtonElement)!;
 
-    propertiesBtn.addEventListener("click", () => {
-        chat.classList.toggle("active-tab");
-        properties.classList.toggle("active-tab");
+    const expandBtn = (document.getElementById("expand") as HTMLButtonElement)!;
+    // const collapseBtn = (document.getElementById("collapse") as HTMLButtonElement)!;
 
-        propertiesBtn.classList.toggle("active");
-        chatBtn.classList.toggle("active");
-    });
+    //Color inputs
+    const strokeColorInput = (document.getElementById(
+        "stroke-color"
+    ) as HTMLInputElement)!;
+    strokeColorInput.value = rightPaneProperties.strokeStyle;
 
-    chatBtn.addEventListener("click", () => {
-        chat.classList.toggle("active-tab");
-        properties.classList.toggle("active-tab");
+    const fillColorInput = (document.getElementById(
+        "fill-color"
+    ) as HTMLInputElement)!;
+    fillColorInput.value = rightPaneProperties.fillStyle;
 
-        propertiesBtn.classList.toggle("active");
-        chatBtn.classList.toggle("active");
-    });
+    //Select input
+    const lineWidthInput = (document.getElementById(
+        "line-width"
+    ) as HTMLSelectElement)!;
 
-    expandBtn.addEventListener("click", () => {
-        rightPane.classList.toggle("expand");
-        let t = expandBtn.dispatchEvent(Collapse);
-        console.log("T:", t);
-        rightPaneExpanded = false;
-        canvas?.classList.add("full-canvas");
-        canvas.width = 2000;
-    });
+    attachEventListeners();
+
+    function attachEventListeners() {
+        propertiesBtn.addEventListener("click", () => {
+            chatTab.classList.toggle("active-tab");
+            propertiesTab.classList.toggle("active-tab");
+
+            propertiesBtn.classList.toggle("active");
+            chatBtn.classList.toggle("active");
+        });
+
+        chatBtn.addEventListener("click", () => {
+            chatTab.classList.toggle("active-tab");
+            propertiesTab.classList.toggle("active-tab");
+
+            propertiesBtn.classList.toggle("active");
+            chatBtn.classList.toggle("active");
+        });
+
+        expandBtn.addEventListener("click", () => {
+            rightPane.classList.toggle("expand");
+            rightPaneExpanded = false;
+            expandBtn.dispatchEvent(collapse);
+        });
+
+        lineWidthInput.addEventListener("change", (e: Event) => {
+            const target = (e.target as HTMLSelectElement)!;
+            rightPaneProperties.lineWidth = Number.parseFloat(target.value);
+            raiseRightPanePropertiesChanged();
+        });
+
+        strokeColorInput.addEventListener("change", (e: Event) => {
+            const target = (e.target as HTMLInputElement)!;
+            rightPaneProperties.strokeStyle = target.value;
+            raiseRightPanePropertiesChanged();
+        });
+
+        fillColorInput.addEventListener("change", (e: Event) => {
+            const target = (e.target as HTMLInputElement)!;
+            rightPaneProperties.fillStyle = target.value;
+            raiseRightPanePropertiesChanged();
+        });
+    }
+
+    function raiseRightPanePropertiesChanged() {
+        rightPane.dispatchEvent(propertiesChange);
+    }
 }
 
 export { createRightPane, rightPaneExpanded };
+export type { RightPaneProperties };
