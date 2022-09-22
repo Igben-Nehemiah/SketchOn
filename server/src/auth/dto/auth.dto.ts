8a1eb878986aa695f839553ad00af35ff7e987e6
nehemiah <igbennehemiah@gmail.com>
2022-09-22 19:04:37 +0100

    Created AuthDto

---
+++ server/src/auth/dto/auth.dto.ts
@@ -0,0 +1,15 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class AuthDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
