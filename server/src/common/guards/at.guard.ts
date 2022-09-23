195a5516d92653b04afd6df947fbc7bd65fb0f74
nehemiah <igbennehemiah@gmail.com>
2022-09-23 12:15:16 +0100

    Renamed guard

--- server/src/common/guards/at.guard.ts
+++ server/src/common/guards/at.guard.ts
@@ -5,21 +5,15 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class AtGuard extends AuthGuard('jwt') {
-  constructor(
-    private readonly reflector: Reflector,
-  ) {
+  constructor(private readonly reflector: Reflector) {
     super();
   }
 
   canActivate(context: ExecutionContext) {
-    const isPublic =
-      this.reflector.getAllAndOverride(
-        'isPublic',
-        [
-          context.getHandler(),
-          context.getClass(),
-        ],
-      );
+    const isPublic = this.reflector.getAllAndOverride('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
     if (isPublic) {
       return true;
