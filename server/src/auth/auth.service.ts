550af43c9a05930776dcfb10c4e1c01fc61ae5fb
nehemiah <igbennehemiah@gmail.com>
2022-09-23 12:17:46 +0100

    Used environment variable

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -6,143 +6,95 @@ import { Tokens } from './types';
 import { JwtService } from '@nestjs/jwt/dist';
 import { ForbiddenException } from '@nestjs/common/exceptions';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ConfigService } from '@nestjs/config/dist';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly jwtService: JwtService,
+    private readonly config: ConfigService,
   ) {}
-  async signupLocal(
-    dto: AuthDto,
-  ): Promise<Tokens> {
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
     try {
-      const hash = await this.hashData(
-        dto.password,
-      );
+      const hash = await this.hashData(dto.password);
 
-      const newUser =
-        await this.prisma.user.create({
-          data: {
-            email: dto.email,
-            hash,
-          },
-        });
+      const newUser = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-      const tokens = await this.getTokens(
-        newUser.id,
-        newUser.email,
-      );
+      const tokens = await this.getTokens(newUser.id, newUser.email);
 
-      await this.updateRtHash(
-        newUser.id,
-        tokens.refresh_token,
-      );
+      await this.updateRtHash(newUser.id, tokens.refresh_token);
       return tokens;
     } catch (error) {
-      if (
-        error instanceof
-        PrismaClientKnownRequestError
-      ) {
+      if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
-          throw new ForbiddenException(
-            'Credentials taken',
-          );
+          throw new ForbiddenException('Credentials taken');
         }
         throw error;
       }
     }
   }
 
-  async signinLocal(
-    dto: AuthDto,
-  ): Promise<Tokens> {
-    const user =
-      await this.prisma.user.findUnique({
-        where: {
-          email: dto.email,
-        },
-      });
+  async signinLocal(dto: AuthDto): Promise<Tokens> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
 
     if (!user) {
-      throw new ForbiddenException(
-        'Invalid credentials',
-      );
+      throw new ForbiddenException('Invalid credentials');
     }
 
-    const hashMatches = await bcrypt.compare(
-      dto.password,
-      user.hash,
-    );
+    const hashMatches = await bcrypt.compare(dto.password, user.hash);
 
     if (!hashMatches) {
-      throw new ForbiddenException(
-        'Invalid credentials',
-      );
+      throw new ForbiddenException('Invalid credentials');
     }
 
-    const tokens = await this.getTokens(
-      user.id,
-      user.email,
-    );
-    await this.updateRtHash(
-      user.id,
-      tokens.refresh_token,
-    );
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
     return tokens;
   }
 
   async logout(userId: number) {
-    const user =
-      await this.prisma.user.updateMany({
-        where: {
-          id: userId,
-          hashedRt: {
-            not: null,
-          },
+    const user = await this.prisma.user.updateMany({
+      where: {
+        id: userId,
+        hashedRt: {
+          not: null,
         },
-        data: {
-          hashedRt: null,
-        },
-      });
+      },
+      data: {
+        hashedRt: null,
+      },
+    });
   }
 
-  async refreshTokens(
-    userId: number,
-    rt: string,
-  ): Promise<Tokens> {
-    const user =
-      await this.prisma.user.findUnique({
-        where: {
-          id: userId,
-        },
-      });
+  async refreshTokens(userId: number, rt: string): Promise<Tokens> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
 
     if (!user || !user.hashedRt) {
-      throw new ForbiddenException(
-        'Access denied',
-      );
+      throw new ForbiddenException('Access denied');
     }
 
-    const hashMatches = await bcrypt.compare(
-      rt,
-      user.hashedRt,
-    );
+    const hashMatches = await bcrypt.compare(rt, user.hashedRt);
 
     if (!hashMatches) {
-      throw new ForbiddenException(
-        'Invalid refresh token',
-      );
+      throw new ForbiddenException('Invalid refresh token');
     }
 
-    const tokens = await this.getTokens(
-      userId,
-      user.email,
-    );
-    await this.updateRtHash(
-      user.id,
-      tokens.refresh_token,
-    );
+    const tokens = await this.getTokens(userId, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
 
     return tokens;
   }
@@ -151,10 +103,7 @@ export class AuthService {
     return await bcrypt.hash(data, 10);
   }
 
-  async getTokens(
-    userId: number,
-    email: string,
-  ): Promise<Tokens> {
+  async getTokens(userId: number, email: string): Promise<Tokens> {
     const [at, rt] = await Promise.all([
       this.jwtService.signAsync(
         {
@@ -162,8 +111,10 @@ export class AuthService {
           email,
         },
         {
-          secret: 'at-secret',
-          expiresIn: 60 * 15,
+          secret: this.config.get('ACCESS_TOKEN_SECRET'),
+          expiresIn: parseInt(
+            this.config.get('ACCESS_TOKEN_EXPIRATION'),
+          ),
         },
       ),
       this.jwtService.signAsync(
@@ -172,8 +123,10 @@ export class AuthService {
           email,
         },
         {
-          secret: 'rt-secret',
-          expiresIn: 60 * 60 * 24 * 7,
+          secret: this.config.get('REFRESH_TOKEN_SECRET'),
+          expiresIn: parseInt(
+            this.config.get('REFRESH_TOKEN_EXPIRATION'),
+          ),
         },
       ),
     ]);
