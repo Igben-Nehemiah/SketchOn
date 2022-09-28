480cfc617d365ec50c7fac1de54c716b00e286ec
nehemiah <igbennehemiah@gmail.com>
2022-09-28 10:28:20 +0100

    Increased maxbuffersize

--- server/src/drawing/drawing.gateway.ts
+++ server/src/drawing/drawing.gateway.ts
@@ -2,7 +2,11 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { MESSAGE_TO_CLIENT, MESSAGE_TO_SERVER } from './constants';
 
-@WebSocketGateway({ path: '/drawing' })
+@WebSocketGateway({
+  path: '/drawing',
+  cors: { origin: 'http://127.0.0.1:5173' },
+  maxHttpBufferSize: 4e6,
+})
 export class DrawingGateway {
   @SubscribeMessage(MESSAGE_TO_SERVER)
   handleMessage(client: Socket, payload: any) {
